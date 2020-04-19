b5973e2080046f64ed1ef0b36aabb24b8909a7ab
Brandon Nolan <brandon.nolan@stratahealth.com>
2020-04-19 11:15:33 -0600

    saving css hexagons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Client } from 'boardgame.io/react';
+import CatanBoard from './CatanBoard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Catan = {
+  setup: () => ({ cells: Array(9).fill(null) }),
+
+  moves: {
+    clickCell: (G, ctx, id) => {
+      G.cells[id] = ctx.currentPlayer;
+    },
+  },
+};
+
+const App = Client({
+  game: Catan,
+  board: CatanBoard});
 
 export default App;
