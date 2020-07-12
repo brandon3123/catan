7b1876c5f0d2bdf88b46b416bbdc9135fff1756d
Brandon Nolan <brandon.nolan@stratahealth.com>
2020-07-12 15:10:53 -0600

    multiplayer added....weird

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React from 'react';
+import { Local } from 'boardgame.io/multiplayer';
 import { Client } from 'boardgame.io/react';
 import CatanBoard from './CatanBoard';
 import Catan from "./CatanGame";
 
-const App = Client({
+const CatanClient = Client({
   game: Catan,
   board: CatanBoard,
-  numPlayers:4
+  numPlayers:4,
+  multiplayer: Local()
   // debug:false
 });
+
+const App = () => (
+    <div>
+      <CatanClient playerID="0"/>
+      <CatanClient playerID="1"/>
+      <CatanClient playerID="2"/>
+      <CatanClient playerID="3"/>
+    </div>
+)
 
 export default App;
